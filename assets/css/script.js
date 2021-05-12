98da23a133b0ff0f0493e1ebc1018b773d07fb90
Ana Ea <aabad0464@gmail.com>
2021-05-12 09:36:23 -0600

    added reset function

--- assets/css/script.js
+++ assets/css/script.js
@@ -90,3 +90,22 @@ function clearScore() {
 
     resetGame();
 }
+
+//Function Reset 
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    let quizContent = 
+`<h1>JavaScript Quiz</h1>
+<h3>Click to play!</h3>
+<button onclick="start()">Start!</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+

b30e1093a6a89dcc802e133f0f798af3406d80d1
Ana Ea <aabad0464@gmail.com>
2021-05-12 09:31:14 -0600

    storing to local storage

--- assets/css/script.js
+++ assets/css/script.js
@@ -64,3 +64,21 @@ function endGame() {
     document.getElementById("quizBody").innerHTML = quizContent;
 }
 
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName", document.getElementById('name').value);
+    getScore();
+}
+
+
+function getScore() {
+    let quizContent = `
+<h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+<h1>` + localStorage.getItem("highscore") + `</h1><br> 
+
+<button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+
+`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}

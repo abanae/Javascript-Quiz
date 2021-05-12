f943c17a97630a6456f7455579ee517a0fb38c68
Ana Ea <aabad0464@gmail.com>
2021-05-12 06:37:32 -0600

    submitted questions

---
+++ assets/css/script.js
@@ -0,0 +1,27 @@
+//Value of Questions    
+let questions = [{
+    title: `Inside the HTML document, where do you place your JavaScript code?`,
+    choices: ["inside script element", "inside head element", "inside footer element", "inside link element"],
+    answer: "inside script element"
+},
+{
+    title: "What operator is used to assign a value to a declared variable?",
+    choices: ["Question mark (?)", "Colon (:)", "Equal sign (=)", "Double-equal (==)"],
+    answer: "Equal sign (=)"
+},
+{
+    title: "What are the six primitive data types in JavaScript?",
+    choices: ["string, number, boolean, bigInt, symbol, undefined", "string, num, falsy, bigInt, symbol, undefined", "sentence, float, data, bigInt, symbol, undefined", "sentence, int, truthy, bigInt, symbol, undefined"],
+    answer: "string, number, boolean, bigInt, symbol, undefined"
+},
+{
+    title: "How do we declare a conditional statement in JavaScript?",
+    choices: ["for loop", "boolean", "unshift", "if...else"],
+    answer: "if...else"
+},
+{
+    title: "From the given array which index is the letter 'b' on? ['a', 'b', 'c', 'd']",
+    choices: ["2", "0", "1", "3"],
+    answer: "1"
+}
+]
